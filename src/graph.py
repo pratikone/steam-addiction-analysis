8d96046878b0b4511df274a78f00bb5ef4525665
Pratik Anand <pratik.preet@gmail.com>
2017-11-11 23:21:19 -0500

    changed graph fetching SQL and scale

--- src/graph.py
+++ src/graph.py
@@ -1,14 +1,18 @@
 import networkx as nx
 import sql_fetch
 import pickle
+import matplotlib.pyplot as plt
 
 def create_graph( given_list ) :
     G = nx.Graph()
     for user_obj in given_list :
         user = user_obj.user_id
         games_id = user_obj.game_id_list
-        for game in games_id :
-            G.add_edge( user, game )
+        if type(games_id) == type([]) :  #if it is a list
+            for game in games_id : 
+                G.add_edge( user, game )
+        else : # if it is an entry
+            G.add_edge( user, games_id )
     return G
 
 
@@ -36,16 +40,15 @@ if __name__ == '__main__' :
     '''
     Uncomment the lines below to fetch from database 
 
-    
     friends_list, user_games_list = sql_fetch.init()
     G  = create_graph(user_games_list)
     add_edges_friends(G, friends_list)
-    print(G.number_of_edges())
     achaar_it(G, "achaar.p")
-    '''
-
+    
+'''
     G = unachaar_it("achaar.p")
-    print(G.number_of_edges())
-    print(G.number_of_nodes())
-
-
+    print("edges", G.number_of_edges())
+    print("nodes", G.number_of_nodes())
+    # print(nx.betweenness_centrality(G))
+    # print( nx.number_connected_components(G))
+    print(sorted(nx.triangles(G).values()))
--- src/sql_fetch.py
+++ src/sql_fetch.py
@@ -52,16 +52,13 @@ def get_user_id_from_friends( friends ) :
      user_list.append(Steam_User(entry.user_id_b))
   return user_list
 
-
-
-
 def get_games_list( connection, users ) :
   try :
     user_games_list = []
     for user in users :
       my_good_list = []
       cur = connection.cursor()
-      cur.execute("SELECT * FROM steam.games_1 where steamid='{}' LIMIT 0, 500 ;".format(user.user_id))
+      cur.execute("SELECT * FROM steam.games_1 where steamid='{}' LIMIT 10000, 500;".format(user.user_id))
       op = cur.fetchall()
       for entry in op :
         my_good_list.append(entry[1])
@@ -83,7 +80,7 @@ def get_user_via_friends( connection ) :
   try :
     friends_list = []
     cur = connection.cursor()
-    cur.execute("SELECT * FROM steam.friends LIMIT 0, 500 ;")
+    cur.execute("SELECT * FROM steam.friends LIMIT 100000, 5000;")
     op = cur.fetchall()
     for entry in op :
       user = entry[0]
@@ -99,14 +96,47 @@ def get_user_via_friends( connection ) :
       sys.exit(1)
 
 
+def get_user_and_games_together(connection) :
+  try :
+    friends_list = []
+    user_games_list = []
+    cur = connection.cursor()
+    cur.execute("SELECT f.steamid_a, f.steamid_b, f.friend_since, g.steamid, g.appid from steam.friends as f,\
+                           steam.games_1 as g where g.steamid = f.steamid_a or g.steamid = f.steamid_b LIMIT 0, 5000000;")
+    op = cur.fetchall()
+    for entry in op :
+      user = entry[0]
+      another_user = entry[1]
+      friends_since = entry[2]
+      game_user = entry[3]
+      game_id = entry[4]
+
+      friends_obj = Steam_Friends(user_id_a = user, user_id_b = another_user, since=friends_since )
+      friends_list.append( friends_obj )
+      if user == game_user :
+        entry_user = user
+      else :
+        entry_user = another_user
+      user_game = Steam_Games(entry_user, game_id)
+      user_games_list.append(user_game)
+    return friends_list, user_games_list
+  except mdb.Error as e:
+      print( "Error %d: %s" % (e.args[0],e.args[1]))
+      if connection:    
+        connection.close()
+      sys.exit(1)
+
+
 
 
 
 def init() :
   connection = create_connection()
   # user_list = get_user_id( connection )
-  friends_list = get_user_via_friends( connection )
-  user_games_list = get_games_list(connection, get_user_id_from_friends( friends_list) )
+  # friends_list = get_user_via_friends( connection )
+  # user_games_list = get_games_list(connection, get_user_id_from_friends( friends_list) )
+  
+  friends_list, user_games_list = get_user_and_games_together( connection )
   return friends_list, user_games_list
 
 
